537c5f401952f59e7c0dfcf0ab0d9663f5b32d10
gvbazhenov <43088667+gvbazhenov@users.noreply.github.com>
2019-02-12 14:54:09 +0300

    Update sinking.py

--- sinking.py
+++ sinking.py
@@ -1,18 +1,21 @@
 import random
 
+def swap(seq, index_left, index_right):
+    seq[index_left], seq[index_right] = seq[index_right], seq[index_left]
+
 def sinking_sort(sequence):
     bias = 0
     for out_iter in range(len(sequence) - 1 - bias):
+        swapped = False
         for in_iter in range(len(sequence) - 1 - out_iter):
             if sequence[in_iter] > sequence[in_iter + 1]:
-                temp = sequence[in_iter]
-                sequence[in_iter] = sequence[in_iter + 1]
-                sequence[in_iter + 1] = temp
+                swap(sequence, in_iter, in_iter + 1)
+                swapped = True
                 bias += 1
-        if not bias:
+        if not swapped:
             break
 
-sequence = [random.randint(1, 100) for i in range(0, 10)]
+sequence = [random.randint(1, 100) for it in range(0, 10)]
 
 print(sequence)
 sinking_sort(sequence)
