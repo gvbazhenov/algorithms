852b6d8192d272a4b1d22a61b26e98c317af1353
gvbazhenov <43088667+gvbazhenov@users.noreply.github.com>
2019-02-12 15:38:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,8 +19,7 @@ int main()
         vector.push_back(distribution(generator));
 
     printSequence(vector); // printing the original sequence
-    //sinkingSort(vector);   // proceeding the sorting
-    insertionSort(vector);
+    insertionSort(vector); // proceeding the sorting
     printSequence(vector); // printing the sorted sequence
 
     return 0;
@@ -31,30 +30,6 @@ void printSequence(std::vector<int> &vector)
     for (std::vector<int>::iterator iter = vector.begin(); iter != vector.end(); iter++)
         std::cout << *iter << ' ';
     std::cout << std::endl;
-}
-
-void sinkingSort(std::vector<int> &vector)
-{
-    int temp = 0;
-    bool swapped = 0;
-    size_t bias = 0;
-
-    for (size_t outIter = 0; outIter != vector.size() - 1 - bias; outIter++)
-    {
-        swapped = false;
-        for (size_t inIter = 0; inIter != vector.size() - 1 - outIter; inIter++)
-        {
-            if (vector[inIter] > vector[inIter + 1]) // checking the opportunity to swap elements
-            {
-                temp = vector[inIter];
-                vector[inIter] = vector[inIter + 1];
-                vector[inIter + 1] = temp;
-                swapped = true;
-                bias++;
-            }
-        }
-        if (!swapped) break; // observing the case of sorted sequence
-    }
 }
 
 void insertionSort(std::vector<int> &vector)
