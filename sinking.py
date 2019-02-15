21312812074bc7b7ee46d7165745960c102d334c
gvbazhenov <43088667+gvbazhenov@users.noreply.github.com>
2019-02-15 20:38:50 +0300

    Update sinking.py

--- sinking.py
+++ sinking.py
@@ -1,15 +1,12 @@
 import random
 
-def swap(seq, index_left, index_right):
-    seq[index_left], seq[index_right] = seq[index_right], seq[index_left]
-
 def sinking_sort(sequence):
     bias = 0
     for out_iter in range(len(sequence) - 1 - bias):
         swapped = False
         for in_iter in range(len(sequence) - 1 - out_iter):
-            if sequence[in_iter] > sequence[in_iter + 1]:
-                swap(sequence, in_iter, in_iter + 1)
+            if sequence[in_iter] > sequence[in_iter + 1]:  # checking the opportunity to swap elements
+                sequence[in_iter], sequence[in_iter + 1] = sequence[in_iter + 1], sequence[in_iter]
                 swapped = True
                 bias += 1
         if not swapped:
