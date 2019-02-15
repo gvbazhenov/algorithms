158301d583c3562ba01972314e4dfeec62958e3d
gvbazhenov <43088667+gvbazhenov@users.noreply.github.com>
2019-02-15 20:42:42 +0300

    Update and rename script.py to insertion.py

--- insertion.py
+++ insertion.py
@@ -1,17 +1,5 @@
 import random
 
-def sinking_sort(sequence):
-    bias = 0
-    for out_iter in range(len(sequence) - 1 - bias):
-        swapped = False
-        for in_iter in range(len(sequence) - 1 - out_iter):
-            if sequence[in_iter] > sequence[in_iter + 1]:  # checking the opportunity to swap elements
-                sequence[in_iter], sequence[in_iter + 1] = sequence[in_iter + 1], sequence[in_iter]
-                swapped = True
-                bias += 1
-        if not swapped:
-            break
-
 def insertion_sort(sequence):
     for out_iter in range(2, len(sequence)):
         key = sequence[out_iter]
@@ -28,3 +16,4 @@ sequence = [random.randint(1, 100) for it in range(0, 10)]
 print(sequence)           # printing the original sequence
 insertion_sort(sequence)  # proceeding the sorting
 print(sequence)           # printing the sorted sequence
+
