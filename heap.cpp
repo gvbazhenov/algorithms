4bc5961b51f78e2fdef7074bd8d6e0f5aff9824d
gvbazhenov <43088667+gvbazhenov@users.noreply.github.com>
2019-09-20 19:59:16 +0300

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,39 @@
+struct Heap {
+    std::vector<int> data;
+    Heap(std::vector<int>& source);
+    void recreate(int position, int size);
+    void sort();
+};
+
+Heap::Heap(std::vector<int>& source) {
+    data = source;
+    for (int iter = (int)data.size() / 2; iter > -1; iter--)
+        recreate(iter, (int)data.size());
+}
+
+void Heap::recreate(int position, int size) {
+    int left = 2 * position + 1, right = 2 * position + 2, greatest = position;
+    
+    while (true) {
+        if (left < size and data[left] > data[greatest])
+            greatest = left;
+        if (right < size and data[right] > data[greatest])
+            greatest = right;
+        
+        if (position == greatest) break;
+        std::swap(data[greatest], data[position]);
+        position = greatest;
+        
+        left = 2 * position + 1;
+        right = 2 * position + 2;
+        greatest = position;
+    }
+}
+
+void Heap::sort() {
+    for (int iter = (int)data.size() - 1; iter > 0; iter--) {
+        std::swap(data[0], data[iter]);
+        recreate(0, iter);
+    }
+}
+
